56acc0577919b56c95c86d8245c544c06cfae231
TestLeafPages <sarath.muthuswamy@testleaf.com>
2020-07-25 19:47:56 +0530

    Update module.js

--- week2Day4/module.js
+++ week2Day4/module.js
@@ -1,5 +1,8 @@
 /* export - make ur variable or function or the entire class itself
 to be accessible outside the current module(file)
+
+node version - v.12.18.2
+
 Two types
 1) named export - multiple 
 2) default export - single occurence in the entire file
